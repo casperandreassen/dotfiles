2953086de47978257e67b46242a4f39a8a2553de
casper <ca.andreassen@outlook.com>
2025-06-08 21:33:55 +0200

    feat: change theme to gruvbox

---
+++ nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,11 @@
+return {
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = {
+    terminal_colors = true,
+  } },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
